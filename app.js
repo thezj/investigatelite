8fc636efd47eea638ad565e28aab286374a9592a
thezj <thezj@vip.qq.com>
2017-11-20 18:13:21 +0800

    添加分类搜索，price字段，购物车样式

--- app.js
+++ app.js
@@ -18,8 +18,17 @@ let books = require('./book.js').books
 app.post('/getbook', function (req, res) {
     let times = Number(req.body.gettime)
     let filters = req.body.filter
+    let classify = req.body.classify
     let filterbooks = books.filter(i => {
-        return i.bookname.indexOf(filters) > -1
+        if (classify == '全部') {
+            if (i.bookName.indexOf(filters) > -1) {
+                return true
+            }
+        } else {
+            if (i.bookName.indexOf(filters) > -1 && i.publishing == classify) {
+                return true
+            }
+        }
     })
     let exportbook = filterbooks.slice(times * 100, times * 100 + 100)
     res.send(JSON.stringify(exportbook))
@@ -30,15 +39,24 @@ app.post('/savebook', function (req, res) {
     fs.readFile('./static/selectedbook.json', 'utf8', function (err, data) {
         if (err) console.log(err);
         data = JSON.parse(data);
-        let selectedbook = JSON.parse(req.body.books)
-        selectedbook.map(i => {
-            if (data[i.subscribeCode]) {
-                data[i.subscribeCode] = Number(data[i.subscribeCode]) + 1
-            } else {
-                data[i.subscribeCode] = 1
-            }
-        })
+        let num = (Math.random() + '').replace('.', '')
+        data[num] = {}
+        data[num]['time'] = new Date().getTime()
+        data[num]['books'] = JSON.parse(req.body.books)
         fs.writeFileSync('./static/selectedbook.json', JSON.stringify(data))
         res.send('ok')
+    });
+});
+
+app.post('/isexist', function (req, res) {
+    let member = req.body.member
+    fs.readFile('./static/selectedbook.json', 'utf8', function (err, data) {
+        if (err) console.log(err);
+        data = JSON.parse(data);
+        if (!data[member]) {
+            res.send('ok')
+        } else {
+            res.send('exist')
+        }
     });
 });
