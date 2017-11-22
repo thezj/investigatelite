08ce4d28b9cb96fd34d95e9f97ca2b44daa3baed
thezj <thezj@vip.qq.com>
2017-11-22 09:35:56 +0800

    修改保存方式轮询保存json

--- app.js
+++ app.js
@@ -34,18 +34,31 @@ app.post('/getbook', function (req, res) {
     res.send(JSON.stringify(exportbook))
 });
 
+let savebookstack = []
+
+let savebookloop = i => {
+    if (savebookstack.length != 0) {
+        let book = savebookstack.pop()
+        fs.readFile('./static/selectedbook.json', 'utf8', function (err, data) {
+            if (err) console.log(err);
+            data = JSON.parse(data);
+            data.push(book)
+            fs.writeFileSync('./static/selectedbook.json', JSON.stringify(data))
+            setTimeout(savebookloop, 500)
+        });
+    }
+    setTimeout(savebookloop, 500)
+}
+savebookloop()
+
 app.post('/savebook', function (req, res) {
-    let member = req.body.member
-    fs.readFile('./static/selectedbook.json', 'utf8', function (err, data) {
-        if (err) console.log(err);
-        data = JSON.parse(data);
-        let note = {}
-        note['time'] = new Date().getTime()
-        note['books'] = JSON.parse(req.body.books)
-        data.push(note)
-        fs.writeFileSync('./static/selectedbook.json', JSON.stringify(data))
-        res.send('ok')
-    });
+
+    let note = {}
+    note['time'] = new Date().getTime()
+    note['books'] = JSON.parse(req.body.books)
+    savebookstack.push(note)
+    res.send('ok')
+
 });
 
 // app.post('/isexist', function (req, res) {
